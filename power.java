67a06d7dee5405356f8c3631081f18e85fa86ef7
ozgurefe <ozgrefe@gmail.com>
2017-02-24 23:33:34 +0300

    power fonksiyonu yazıldı

---
+++ power.java
@@ -0,0 +1,23 @@
+class power
+{
+
+      void pow(int c, int d)
+     {       
+              int n=1;
+              for(int i=0;i<d;i++)
+              {
+                       n=c*n;
+              }
+              System.out.println(n);
+     } 
+
+
+
+    public static void main(String arg[])
+    {
+    
+    b=sc.nextInt();
+        power c=new power();
+        c.pow(2,16);
+    }
+}
